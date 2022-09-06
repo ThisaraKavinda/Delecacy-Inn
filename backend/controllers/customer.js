eb027e427d17eef647c0d01234888667bc967ed5
hirusha2577 <56668123+hirusha2577@users.noreply.github.com>
2022-09-07 00:38:14 +0530

    #2 Create node server, react app, customer backend, customer frontend

---
+++ backend/controllers/customer.js
@@ -0,0 +1,86 @@
+
+import { CustomerModel } from '../models/customer.js';
+
+
+export const addCustomer = async (req, res) => {
+    console.log(req.body);
+    const customer = new CustomerModel({
+        name: req.body.name,
+        email: req.body.email,
+        address: req.body.address,
+        nic: req.body.nic,
+        contactNo: req.body.contactNo,
+        state: req.body.state
+    });
+    const details = await customer.save();
+    if (details) {
+        res.send({
+            status: true,
+            details: details
+        });
+    } else {
+        res.send({
+            status: false,
+        });
+    }
+}
+
+export const getAllCustomers = async (req, res) => {
+    const customers = await CustomerModel.find({});
+    res.send(customers);
+}
+
+export const getAllCustomersCount = async (req, res) => {
+    const customers = await CustomerModel.find().count();
+    res.send(String(customers));
+}
+
+
+export const deleteCustomer = async (req, res) => {
+    const customer = await CustomerModel.findOneAndDelete({ _id: req.body.id });
+    res.send(customer);
+}
+
+export const editCustomer = async (req, res) => {
+    try {
+        console.log(req);
+        const customer = await CustomerModel.findOneAndUpdate(
+            {
+                _id: req.body._id
+            },
+            {
+                _id: req.body._id,
+                name: req.body.name,
+                email: req.body.email,
+                address: req.body.address,
+                nic: req.body.nic,
+                contactNo: req.body.contactNo,
+                state: req.body.state
+            },
+            {
+                new:true
+            }
+            );
+
+        if (customer) {
+            res.send({
+                status: true,
+                details: vehicle  
+            });
+        } else {
+            res.send({
+                status: false,
+            });
+        }
+
+    } catch (error) {
+        console.log(error.messaga)
+    }
+}
+
+
+
+export const getSelectedCustomer = async (req, res) => {
+    const customer = await CustomerModel.findOne({ _id: req.body.id });
+    res.send(customer);
+}
